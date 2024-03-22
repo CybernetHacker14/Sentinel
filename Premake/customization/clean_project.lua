b02ff57a7c253695120fce7881815fd9e400897f
CybernetHacker14 <ujjwalraut.14@gmail.com>
2024-03-22 16:40:43 +0530

    [Premake] Updated clean command to not delete all VS files

--- Premake/customization/clean_project.lua
+++ Premake/customization/clean_project.lua
@@ -9,9 +9,27 @@ newaction {
 		os.rmdir("./.vs")
 		
 		os.remove("*.sln")
-		os.remove("**.vcxproj")
-		os.remove("**.vcxproj.filters")
-		os.remove("**.vcxproj.user")
+
+		os.remove("Sentinel/*.vcxproj")
+		os.remove("Sentinel/*.vcxproj.filters")
+		os.remove("Sentinel/*.vcxproj.user")
+
+		os.remove("Sentinel/Vendor/cglm/*.vcxproj")
+		os.remove("Sentinel/Vendor/cglm/*.vcxproj.filters")
+		os.remove("Sentinel/Vendor/cglm/*.vcxproj.user")
+
+		os.remove("Sentinel/Vendor/glfw/*.vcxproj")
+		os.remove("Sentinel/Vendor/glfw/*.vcxproj.filters")
+		os.remove("Sentinel/Vendor/glfw/*.vcxproj.user")
+
+		os.remove("Premake/*.vcxproj")
+		os.remove("Premake/*.vcxproj.filters")
+		os.remove("Premake/*.vcxproj.user")
+
+		os.remove("Sandbox/*.vcxproj")
+		os.remove("Sandbox/*.vcxproj.filters")
+		os.remove("Sandbox/*.vcxproj.user")
+
 		print("Done")
 	end
 }
