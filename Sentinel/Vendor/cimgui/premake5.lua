7276b5a647dd740b17a571c7820dda3329a9a564
CybernetHacker14 <ujjwalraut.14@gmail.com>
2024-06-08 21:27:07 +0530

    [WIP] Restore point

---
+++ Sentinel/Vendor/cimgui/premake5.lua
@@ -0,0 +1,51 @@
+project "cimgui"
+    kind "StaticLib"
+    language "C"
+    cdialect "C11"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "module/cimgui.h",
+        "module/generator/cimgui_impl.h",
+        "module/cimgui.cpp",
+        "module/imgui/imconfig.h",
+        "module/imgui/imgui.h",
+        "module/imgui/imgui.cpp",
+        "module/imgui/imgui_draw.cpp",
+        "module/imgui/imgui_internal.h",
+        "module/imgui/imgui_tables.cpp",
+        "module/imgui/imgui_widgets.cpp",
+        "module/imgui/imstb_rectpack.h",
+        "module/imgui/imstb_textedit.h",
+        "module/imgui/imstb_truetype.h",
+        "module/imgui/imgui_demo.cpp"
+    }
+
+    includedirs
+    {
+        "module/imgui",
+        "%{VendorIncludes.glfw}"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        staticruntime "On"
+
+        files
+        {
+            "module/imgui/backends/imgui_impl_dx11.h",
+            "module/imgui/backends/imgui_impl_glfw.h",
+            "module/imgui/backends/imgui_impl_dx11.cpp",
+            "module/imgui/backends/imgui_impl_glfw.cpp"
+        }
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
